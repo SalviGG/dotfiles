62530bee6cef3c445b0a99a6e2f4a8a685c4f9ef
Salvador Gutiérrez <sgutierrez@minvu.cl>
2025-02-20 16:02:44 -0300

    snacks keybindings and removal of unused plugins

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,20 +4,47 @@ return {
 	lazy = false,
 	---@type snacks.Config
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		bigfile = { enabled = true },
 		dashboard = { enabled = true },
-		explorer = { enabled = false },
+		explorer = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
-		picker = { enabled = false },
+		picker = {
+			enabled = true,
+			---@class snacks.picker.sources.Config
+			sources = {
+				files = {
+					follow = true,
+				},
+			},
+		},
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
 		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
+	},
+	keys = {
+		-- stylua: ignore start
+		-- Top Pickers & Explorer
+			{ "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+		-- Find
+			{ "<leader><leader>", function() Snacks.picker.buffers() end, desc = "Buffers" },
+			{ "<leader>sf", function() Snacks.picker.files() end, desc = "[S]earch [F]iles" },
+		-- Grep
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+			{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+		-- LSP
+			{ "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+			{ "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+			{ "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+			{ "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+			{ "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+
+		-- Others
+			{ "<C>t",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+		-- stylua: ignore end
 	},
 }
