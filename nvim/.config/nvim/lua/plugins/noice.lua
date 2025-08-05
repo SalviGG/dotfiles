f2ede3940e3de68fac52e89f1d322e5107a2ac3b
Salvador Gutiérrez <kei.miskatonic@gmail.com>
2025-08-05 16:05:09 -0400

    Update noice.lua
    Updated to use recommended presets.

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -22,6 +22,11 @@ return {
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
 			},
+			presets = {
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
 		})
 	end,
 }
